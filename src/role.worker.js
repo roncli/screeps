7ff6d7c54a2598cc0a535bddd1b24c786f7b1ac4
Ronald M. Clifford <roncli@roncli.com>
2016-10-06 11:27:43 -0700

    Always go to the controller if there is nothing else to do.

--- src/role.worker.js
+++ src/role.worker.js
@@ -240,13 +240,11 @@ var Cache = require("cache"),
 
             // Check for controllers to upgrade.
             _.forEach(TaskUpgradeController.getTasks(room), (task) => {
-                if (Utilities.creepsWithTask(Cache.creepsInRoom("worker", room), {type: "upgradeController", room: room.name}).length === 0) {
-                    _.forEach(Utilities.objectsClosestToObj(Utilities.creepsWithNoTask(Cache.creepsInRoom("worker", room)), room.controller), (creep) => {
-                        if (task.canAssign(creep, creepTasks)) {
-                            creep.say("Controller");
-                        }
-                    });
-                }
+                _.forEach(Utilities.objectsClosestToObj(Utilities.creepsWithNoTask(Cache.creepsInRoom("worker", room)), room.controller), (creep) => {
+                    if (task.canAssign(creep, creepTasks)) {
+                        creep.say("Controller");
+                    }
+                });
             });
             
             // Attempt to assign harvest task to remaining creeps.

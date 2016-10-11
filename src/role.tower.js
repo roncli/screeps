9baf15b13d1665037d7a4c49f74314ae66cdd1af
Ronald M. Clifford <roncli@roncli.com>
2016-10-10 17:33:23 -0700

    Fix Tower role variable names and allow the tower to heal.

--- src/role.tower.js
+++ src/role.tower.js
@@ -1,9 +1,10 @@
 var Cache = require("cache"),
     Utilities = require("utilities"),
+    TaskHeal = require("task.heal"),
     TaskRangedAttack = require("task.rangedAttack"),
     TaskRepair = require("task.repair"),
 
-    Worker = {
+    Tower = {
         assignTasks: (room) => {
             "use strict";
 
@@ -30,15 +31,24 @@ var Cache = require("cache"),
             // Check for repairs.
             tasks = TaskRepair.getTasks(room);
             if (tasks.length > 0) {
-                if (tasks[0].structure.hits < 5000) {
+                if (tasks[0].structure.hits < 25000) {
                     _.forEach(room.find(FIND_MY_STRUCTURES, {filter: (structure) => structure.structureType === STRUCTURE_TOWER}), (tower) => {
                         tower.repair(tasks[0].structure);
                     });
                     return;
                 }
             }
+
+            // Check for heals.
+            tasks = TaskHeal.getTasks(room);
+            if (tasks.length > 0) {
+                _.forEach(room.find(FIND_MY_STRUCTURES, {filter: (structure) => structure.structureType === STRUCTURE_TOWER}), (tower) => {
+                    tower.heal(tasks[0].ally);
+                });
+                return;
+            }
         }
     };
 
-require("screeps-profiler").registerObject(Worker, "RoleWorker");
-module.exports = Worker;
+require("screeps-profiler").registerObject(Tower, "RoleTower");
+module.exports = Tower;

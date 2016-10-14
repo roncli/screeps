2e4f5c60f801b2b70bf6bae94d175814fd078987
Ronald M. Clifford <roncli@roncli.com>
2016-10-14 02:18:12 -0700

    Check room for spawns.

--- src/room.base.js
+++ src/room.base.js
@@ -162,7 +162,7 @@ Base.prototype.run = function(room) {
 
     // Transfer energy from far link to near link.
     if (Cache.spawnsInRoom(room).length > 0) {
-        links = Utilities.objectsClosestToObj(Cache.linksInRoom(room), Cache.spawnsInRoom[0]);
+        links = Utilities.objectsClosestToObj(Cache.linksInRoom(room), Cache.spawnsInRoom(room)[0]);
         if (links.length > 1 && !links[1].cooldown && links[1].energy > 0) {
             links[1].transferEnergy(links[0]);
         }

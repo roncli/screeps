109c4b1c22e93b969cead2833bc9718ecedcdfc4
Ronald M. Clifford <roncli@roncli.com>
2016-10-19 20:15:16 -0700

    Implementation of console in node.

---
+++ console/index.js
@@ -0,0 +1,67 @@
+var WebSocket = require("ws"),
+    https = require("https"),
+    config = require("./config"),
+    ws = new WebSocket("wss://screeps.com/socket/websocket"),
+    authPost = "email=" + config.user + "&password=" + config.password,
+    req = https.request({
+        host: "screeps.com",
+        path: "/api/auth/signin",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Length": Buffer.byteLength(authPost)
+        }
+    }, function(res) {
+        var signinResponse = "";
+
+        res.on("data", function(chunk) {
+            signinResponse += chunk;
+        });
+
+        res.on("end", function() {
+            var response = JSON.parse(signinResponse),
+                token = response.token,
+                req = https.request({
+                    host: "screeps.com",
+                    path: "/api/auth/me",
+                    method: "GET",
+                    headers: {
+                        "X-Token": token,
+                        "X-Username": config.user
+                    }
+                }, function(res) {
+                    var meResponse = "";
+
+                    res.on("data", function(chunk) {
+                        meResponse += chunk;
+                    });
+
+                    res.on("end", function() {
+                        var response = JSON.parse(meResponse);
+
+                        id = response._id;
+                        ws.send("auth " + token);
+                    });
+                });
+
+            req.end();
+        });
+    }),
+
+    id;
+
+req.write(authPost);
+req.end();
+
+ws.onmessage = function(message) {
+    if (message.data.startsWith("auth ok")) {
+        ws.send("subscribe user:" + id + "/console");
+    }
+
+    try {
+        console.log("\033c");
+        JSON.parse(message.data)[1].messages.log.forEach((l) => {
+            console.log(l);
+        });
+    } catch (err) {}
+};

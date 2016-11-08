d9160a17f3d128224a77c8b30fa67462313c64e9
Ronald M. Clifford <roncli@roncli.com>
2016-11-08 09:19:36 -0800

    Don't spawn miners or storers for the room in another room.

--- src/role.miner.js
+++ src/role.miner.js
@@ -45,7 +45,7 @@ var Cache = require("cache"),
                 energy, count, spawnToUse, name;
 
             // Fail if all the spawns are busy.
-            if (_.filter(Game.spawns, (s) => !s.spawning && !Cache.spawning[s.id]).length === 0) {
+            if (_.filter(Cache.spawnsInRoom(room), (s) => !s.spawning && !Cache.spawning[s.id]).length === 0) {
                 return false;
             }
 
@@ -102,11 +102,9 @@ var Cache = require("cache"),
             }
 
             // Create the creep from the first listed spawn that is available.
-            spawnToUse = _.sortBy(_.filter(Game.spawns, (s) => !s.spawning && !Cache.spawning[s.id]), (s) => s.room.name === room.name ? 0 : 1)[0];
+            spawnToUse = _.filter(Cache.spawnsInRoom(room), (s) => !s.spawning && !Cache.spawning[s.id])[0];
             name = spawnToUse.createCreep(body, undefined, {role: "miner", home: room.name, container: id});
-            if (spawnToUse.room.name === room.name) {
-                Cache.spawning[spawnToUse.id] = true;
-            }
+            Cache.spawning[spawnToUse.id] = true;
 
             // If successful, log it.
             if (typeof name !== "number") {

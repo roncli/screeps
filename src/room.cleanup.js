75b46d9ac4174d270e72bd6a951380d2d3126fc2
Ronald M. Clifford <roncli@roncli.com>
2016-11-23 00:04:45 -0800

    I guess we need some tasks for these guys?

--- src/room.cleanup.js
+++ src/room.cleanup.js
@@ -45,8 +45,30 @@ Cleanup.prototype.run = function(room) {
         },
         remoteDismantle: {
             cleanupTasks: []
+        },
+        dismantle: {
+            tasks: []
         }
     };
+
+    if (Memory.dismantle && Memory.dismantle[room.name] && Memory.dismantle[room.name].length > 0) {
+        _.forEach(Memory.dismantle[room.name], (pos) => {
+            var structures = room.lookForAt(LOOK_STRUCTURES, pos.x, pos.y);
+            if (structures.length === 0) {
+                completed.push(pos);
+            } else {
+                tasks.dismantle.tasks = tasks.dismantle.tasks.concat(_.map(structures, (s) => new TaskDismantle(s.id)));
+            }
+        });
+        _.forEach(completed, (complete) => {
+            _.remove(Memory.dismantle[room.name], (d) => d.x === complete.x && d.y === complete.y);
+        });
+    } else {
+        _.forEach(Cache.creepsInRoom("dismantler", room), (creep) => {
+            creep.memory.role = "worker";
+            creep.memory.home = supportRoom.name
+        });
+    }
 
     if (!room.unobservable) {
         // Find all ramparts.

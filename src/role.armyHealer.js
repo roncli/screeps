79927be3e6d1834a57702b666c98cae5d92087fd
Ronald M. Clifford <roncli@roncli.com>
2016-11-24 22:08:36 -0800

    Allow more than one creep to heal others.

--- src/role.armyHealer.js
+++ src/role.armyHealer.js
@@ -169,7 +169,6 @@ var Cache = require("cache"),
                             if (task.canAssign(creep)) {
                                 creep.say("Heal");
                                 assigned.push(creep.name);
-                                return false;
                             }
                         });
 

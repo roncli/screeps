8a99b08f964a603bbd930e56c216e144a395372a
Ronald M. Clifford <roncli@roncli.com>
2016-11-25 13:25:33 -0800

    Towers do critical repairs under 10,000 hits only.

--- src/role.tower.js
+++ src/role.tower.js
@@ -13,10 +13,11 @@ var Cache = require("cache"),
                 return;
             }
 
-            // Check for critical repairs.
-            if (tasks.repair.criticalTasks.length > 0) {
+            // Check for critical repairs under 10000 hits.
+            filteredTasks = _.filter(tasks.repair.criticalTasks, (t) => t.structure.hits < 10000);
+            if (filteredTasks.length > 0) {
                 _.forEach(Cache.towersInRoom(room), (tower) => {
-                    tower.repair(tasks.repair.criticalTasks[0].structure);
+                    tower.repair(filteredTasks[0].structure);
                 });
                 return;
             }

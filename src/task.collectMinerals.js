654c853122be2e466357e46151d27565bc89598d
Ronald M. Clifford <roncli@roncli.com>
2016-11-29 20:39:40 -0800

    Check length of labs in room.

--- src/task.collectMinerals.js
+++ src/task.collectMinerals.js
@@ -137,7 +137,7 @@ CollectMinerals.getStorageTasks = function(room) {
         amount;
 
     // We only need to transfer from storage to lab when we have both storage and at least 3 labs.
-    if (room.storage && room.memory.labQueue && room.memory.labQueue.type === "create" && room.memory.labQueue.status === "moving" && Cache.labsInRoom(room) >= 3) {
+    if (room.storage && room.memory.labQueue && room.memory.labQueue.type === "create" && room.memory.labQueue.status === "moving" && Cache.labsInRoom(room).length >= 3) {
         _.forEach(room.memory.labQueue.children, (resource) => {
             if (amount = _.sum(_.filter(Cache.labsInRoom(room), (l) => l.mineralType === resource), (l) => l.mineralAmount) < room.memory.labQueue.amount) {
                 tasks.push(new CollectMinerals(room.storage.id, resource, room.memory.labQueue.amount - amount));

cf6de855f27960436a3212dd629b2b67fd602a22
Ronald M. Clifford <roncli@roncli.com>
2016-12-01 01:28:49 -0800

    Get labs from memory.

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -117,7 +117,7 @@ var Cache = require("cache"),
 
             // If not yet boosted, go get boosts.
             _.forEach(_.filter(creepsWithNoTask, (c) => c.memory.labs && c.memory.labs.length > 0), (creep) => {
-                var task = new TaskRally(creep.labs[0]);
+                var task = new TaskRally(creep.memory.labs[0]);
                 task.canAssign(creep);
                 assigned.push(creep.name);
             });

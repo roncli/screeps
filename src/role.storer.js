8d81f38730ebe1baa659ccc6eb4c3e8f6b5bb7b4
Ronald M. Clifford <roncli@roncli.com>
2016-12-09 20:22:43 -0800

    This'll help...

--- src/role.storer.js
+++ src/role.storer.js
@@ -40,7 +40,7 @@ var Cache = require("cache"),
             });
 
             // If we don't have a storer for each container, spawn one.
-            max = Math.ceil((2 * length) / 30) + ((Memory.army && Memory.army.length > 0) ? 1 : 0);
+            max = Math.ceil((2 * length) / 30) + ((Memory.army && _.keys(Memory.army).length > 0) ? 1 : 0);
             if (_.filter(Cache.creepsInRoom("storer", room), (c) => c.spawning || c.ticksToLive >= 300).length < max) {
                 Storer.spawn(room);
             }

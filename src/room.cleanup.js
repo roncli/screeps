95ef7dc2ef463f803cf562b05778fd7d617bf627
Ronald M. Clifford <roncli@roncli.com>
2016-12-11 01:35:53 -0800

    Cleaning up a room needn't involve hostiles.

--- src/room.cleanup.js
+++ src/room.cleanup.js
@@ -77,10 +77,10 @@ Cleanup.prototype.run = function(room) {
         }
 
         // Find all ramparts.
-        ramparts = _.filter(room.find(FIND_HOSTILE_STRUCTURES), (s) => s instanceof StructureRampart);
+        ramparts = _.filter(room.find(FIND_STRUCTURES), (s) => s instanceof StructureRampart);
 
         // Find all structures that aren't under ramparts, divided by whether they have energy or not.
-        structures = _.filter(room.find(FIND_HOSTILE_STRUCTURES), (s) => !(s instanceof StructureRampart) && !(s instanceof StructureController) && (ramparts.length === 0 || s.pos.getRangeTo(Utilities.objectsClosestToObj(ramparts, s)[0]) > 0));
+        structures = _.filter(room.find(FIND_STRUCTURES), (s) => !(s instanceof StructureRampart) && !(s instanceof StructureController) && (ramparts.length === 0 || s.pos.getRangeTo(Utilities.objectsClosestToObj(ramparts, s)[0]) > 0));
         noEnergyStructures = _.filter(structures, (s) => (!s.energy || s.energy === 0) && (!s.store || _.sum(s.store) === 0) && (!s.mineralAmount || s.mineralAmount === 0));
         energyStructures = _.filter(structures, (s) => (s.energy && s.energy > 0) || (s.store && _.sum(s.store) > 0) || (s.mineralAmount && s.mineralAmount > 0));
 

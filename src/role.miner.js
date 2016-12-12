45ac9624dc64ff4b24734a1aeb31dd466dc96e79
Ronald M. Clifford <roncli@roncli.com>
2016-12-11 21:29:52 -0800

    Actually boost miners.

--- src/role.miner.js
+++ src/role.miner.js
@@ -150,6 +150,20 @@ var Cache = require("cache"),
                 return;
             }
 
+            // If not yet boosted, go get boosts.
+            _.forEach(_.filter(creepsWithNoTask, (c) => c.memory.labs && c.memory.labs.length > 0), (creep) => {
+                var task = new TaskRally(creep.memory.labs[0]);
+                task.canAssign(creep);
+                assigned.push(creep.name);
+            });
+
+            _.remove(creepsWithNoTask, (c) => assigned.indexOf(c.name) !== -1);
+            assigned = [];
+
+            if (creepsWithNoTask.length === 0) {
+                return;
+            }
+
             // Attempt to assign mine tasks.
             _.forEach(creepsWithNoTask, (creep) => {
                 var task = new TaskMine();

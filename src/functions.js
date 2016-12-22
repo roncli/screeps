c9d931209948ee4d9484717db8dbc467a01b2ab2
Ronald M. Clifford <roncli@roncli.com>
2016-12-21 19:08:19 -0800

    Fix extractors, too.

--- src/functions.js
+++ src/functions.js
@@ -8,7 +8,7 @@ var Functions = {
     },
     
     filterIsExtractor: (s) => {
-        s instanceof StructureExtractor;
+        return s instanceof StructureExtractor;
     },
     
     filterIsLab: (s) => {

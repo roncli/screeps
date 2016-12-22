b2c52a29a7f5a3fd4d3314f80fd7675feebcbd97
Ronald M. Clifford <roncli@roncli.com>
2016-12-22 16:51:02 +0000

    Optimizations for role.armyDismantler.js.

--- src/role.armyDismantler.js
+++ src/role.armyDismantler.js
@@ -4,18 +4,18 @@ var Cache = require("cache"),
     TaskRally = require("task.rally"),
 
     Dismantler = {
-        checkSpawn: (army) => {
+        checkSpawn: (armyName) => {
             "use strict";
 
-            var count = _.filter(Cache.creepsInArmy("armyDismantler", army), (c) => c.spawning || c.ticksToLive > 300).length, max = Memory.army[army].dismantler.maxCreeps;
+            var count = _.filter(Cache.creepsInArmy("armyDismantler", armyName), (c) => c.spawning || c.ticksToLive > 300).length, max = Memory.army[armyName].dismantler.maxCreeps;
 
             if (count < max) {
-                Dismantler.spawn(army);
+                Dismantler.spawn(armyName);
             }
 
             // Output dismantler count in the report.
             if (max > 0) {
-                Cache.log.army[army].creeps.push({
+                Cache.log.army[armyName].creeps.push({
                     role: "armyDismantler",
                     count: count,
                     max: max
@@ -23,10 +23,13 @@ var Cache = require("cache"),
             }        
         },
         
-        spawn: (army) => {
+        spawn: (armyName) => {
             "use strict";
 
-            var body = [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE],
+            var army = Memory.army[armyName],
+                dismantlerUnits = army.dismantler.units,
+                boostRoom = Game.rooms[army.boostRoom],
+                body = [TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE],
                 count, spawnToUse, name, labsToBoostWith;
 
             // Fail if all the spawns are busy.
@@ -35,37 +38,37 @@ var Cache = require("cache"),
             }
 
             // Create the body of the army.
-            for (count = 0; count < Memory.army[army].dismantler.units; count++) {
+            for (count = 0; count < dismantlerUnits; count++) {
                 body.push(WORK);
                 body.push(MOVE);
             }
 
-            if (Game.rooms[Memory.army[army].boostRoom] && !(labsToBoostWith = Utilities.getLabToBoostWith(Game.rooms[Memory.army[army].boostRoom], 2))) {
+            if (boostRoom && !(labsToBoostWith = Utilities.getLabToBoostWith(boostRoom, 2))) {
                 return false;
             }
 
             // Create the creep from the first listed spawn that is available.
-            spawnToUse = _.filter(Game.spawns, (s) => !s.spawning && !Cache.spawning[s.id] && s.room.energyAvailable >= Utilities.getBodypartCost(body) && s.room.memory.region === Memory.army[army].region)[0];
+            spawnToUse = _.filter(Game.spawns, (s) => !s.spawning && !Cache.spawning[s.id] && s.room.energyAvailable >= Utilities.getBodypartCost(body) && s.room.memory.region === army.region)[0];
             if (!spawnToUse) {
                 return false;
             }
-            name = spawnToUse.createCreep(body, "armyDismantler-" + army + "-" + Game.time.toFixed(0).substring(4), {role: "armyDismantler", army: army, labs: Game.rooms[Memory.army[army].boostRoom] ? _.map(labsToBoostWith, (l) => l.id) : []});
+            name = spawnToUse.createCreep(body, "armyDismantler-" + armyName + "-" + Game.time.toFixed(0).substring(4), {role: "armyDismantler", army: armyName, labs: boostRoom ? _.map(labsToBoostWith, (l) => l.id) : []});
             Cache.spawning[spawnToUse.id] = typeof name !== "number";
 
-            if (typeof name !== "number" && Game.rooms[Memory.army[army].boostRoom]) {
+            if (typeof name !== "number" && boostRoom) {
                 // Set the labs to be in use.
                 labsToBoostWith[0].creepToBoost = name;
                 labsToBoostWith[0].resource = RESOURCE_CATALYZED_GHODIUM_ALKALIDE;
                 labsToBoostWith[0].amount = 30 * 5;
-                Game.rooms[Memory.army[army].boostRoom].memory.labsInUse.push(labsToBoostWith[0]);
+                boostRoom.memory.labsInUse.push(labsToBoostWith[0]);
 
                 labsToBoostWith[1].creepToBoost = name;
                 labsToBoostWith[1].resource = RESOURCE_CATALYZED_ZYNTHIUM_ACID;
-                labsToBoostWith[1].amount = 30 * Memory.army[army].dismantler.units;
-                Game.rooms[Memory.army[army].boostRoom].memory.labsInUse.push(labsToBoostWith[1]);
+                labsToBoostWith[1].amount = 30 * dismantlerUnits;
+                boostRoom.memory.labsInUse.push(labsToBoostWith[1]);
 
                 // If anything is coming to fill the labs, stop them.
-                _.forEach(_.filter(Cache.creepsInRoom("all", Game.rooms[Memory.army[army].boostRoom]), (c) => c.memory.currentTask && c.memory.currentTask.type === "fillMinerals" && _.map(labsToBoostWith, (l) => l.id).indexOf(c.memory.currentTask.id) !== -1), (creep) => {
+                _.forEach(_.filter(Cache.creepsInRoom("all", boostRoom), (c) => c.memory.currentTask && c.memory.currentTask.type === "fillMinerals" && _.map(labsToBoostWith, (l) => l.id).indexOf(c.memory.currentTask.id) !== -1), (creep) => {
                     delete creep.memory.currentTask;
                 });
             }
@@ -73,12 +76,17 @@ var Cache = require("cache"),
             return typeof name !== "number";
         },
 
-        assignTasks: (army, directive, tasks) => {
+        assignTasks: (armyName, directive, tasks) => {
             "use strict";
 
-            var creepsWithNoTask = _.filter(Utilities.creepsWithNoTask(Cache.creepsInArmy("armyDismantler", army)), (c) => !c.spawning),
+            var creepsWithNoTask = _.filter(Utilities.creepsWithNoTask(Cache.creepsInArmy("armyDismantler", armyName)), (c) => !c.spawning),
                 assigned = [],
-                task, structures, structure, healers;
+                army = Memory.army[armyName],
+                stageRoomName = army.stageRoom,
+                attackRoomName = army.attackRoom,
+                attackRoom = Game.rooms[attackRoomName],
+                dismantle = army.dismantle,
+                task, structures, healers;
 
             if (creepsWithNoTask.length === 0) {
                 return;
@@ -101,7 +109,7 @@ var Cache = require("cache"),
                     }
 
                     // Rally to army's building location.
-                    task = new TaskRally(Memory.army[army].buildRoom);
+                    task = new TaskRally(army.buildRoom);
                     _.forEach(creepsWithNoTask, (creep) => {
                         creep.say("Building");
                         task.canAssign(creep);
@@ -109,7 +117,7 @@ var Cache = require("cache"),
                     break;
                 case "staging":
                     // Rally to army's staging location.
-                    task = new TaskRally(Memory.army[army].stageRoom);
+                    task = new TaskRally(stageRoomName);
                     _.forEach(creepsWithNoTask, (creep) => {
                         creep.say("Staging");
                         task.canAssign(creep);
@@ -117,7 +125,7 @@ var Cache = require("cache"),
                     break;
                 case "dismantle":
                     // If we're more than 2 units from the closest healer, run towards it.
-                    healers = Cache.creepsInArmy("armyHealer", army);
+                    healers = Cache.creepsInArmy("armyHealer", armyName);
                     if (healers.length > 0) {
                         _.forEach(creepsWithNoTask, (creep) => {
                             var closest = Utilities.objectsClosestToObj(healers, creep),
@@ -139,9 +147,9 @@ var Cache = require("cache"),
                     }
                     
                     // Return to army's staging location if missing 1000 hits.
-                    if (Memory.army[army].stageRoom !== Memory.army[army].attackRoom) {
-                        task = new TaskRally(Memory.army[army].stageRoom);
-                        _.forEach(_.filter(creepsWithNoTask, (c) => (c.room.name === Memory.army[army].attackRoom || c.pos.x <=1 || c.pos.x >=48 || c.pos.y <= 1 || c.pos.y >= 48) && c.hitsMax - c.hits >= 1000), (creep) => {
+                    if (stageRoomName !== attackRoomName) {
+                        task = new TaskRally(stageRoomName);
+                        _.forEach(_.filter(creepsWithNoTask, (c) => (c.room.name === attackRoomName || c.pos.x <=1 || c.pos.x >=48 || c.pos.y <= 1 || c.pos.y >= 48) && c.hitsMax - c.hits >= 1000), (creep) => {
                             creep.say("Ouch!");
                             task.canAssign(creep);
                             assigned.push(creep.name);
@@ -159,8 +167,8 @@ var Cache = require("cache"),
                     _.remove(creepsWithNoTask, (c) => c.hitsMax - c.hits >= 1000);
 
                     // Dismantle a dismantle location if it can be seen.
-                    if (Game.rooms[Memory.army[army].attackRoom] && Memory.army[army].dismantle.length > 0) {
-                        task = new TaskDismantle(Memory.army[army].dismantle[0]);
+                    if (attackRoom && dismantle.length > 0) {
+                        task = new TaskDismantle(dismantle[0]);
                         _.forEach(creepsWithNoTask, (creep) => {
                             if (task.canAssign(creep)) {
                                 creep.say("Dismantle");
@@ -177,16 +185,16 @@ var Cache = require("cache"),
                     }
 
                     // Rally to army's attack location.
-                    task = new TaskRally(Memory.army[army].attackRoom);
+                    task = new TaskRally(attackRoomName);
                     _.forEach(creepsWithNoTask, (creep) => {
                         task.canAssign(creep);
                     });
                     break;
                 case "attack":
                     // Return to army's staging location if missing 1000 hits.
-                    if (Memory.army[army].stageRoom !== Memory.army[army].attackRoom) {
-                        task = new TaskRally(Memory.army[army].stageRoom);
-                        _.forEach(_.filter(creepsWithNoTask, (c) => (c.room.name === Memory.army[army].attackRoom || c.pos.x <=1 || c.pos.x >=48 || c.pos.y <= 1 || c.pos.y >= 48) && c.hitsMax - c.hits >= 1000), (creep) => {
+                    if (stageRoomName !== attackRoomName) {
+                        task = new TaskRally(stageRoomName);
+                        _.forEach(_.filter(creepsWithNoTask, (c) => (c.room.name === attackRoomName || c.pos.x <=1 || c.pos.x >=48 || c.pos.y <= 1 || c.pos.y >= 48) && c.hitsMax - c.hits >= 1000), (creep) => {
                             creep.say("Ouch!");
                             task.canAssign(creep);
                             assigned.push(creep.name);
@@ -203,8 +211,8 @@ var Cache = require("cache"),
                     // Remove any creeps that need healing.
                     _.remove(creepsWithNoTask, (c) => c.hitsMax - c.hits >= 1000);
 
-                    if (Game.rooms[Memory.army[army].attackRoom]) {
-                        structures = _.sortBy(_.filter(Game.rooms[Memory.army[army].attackRoom].find(FIND_HOSTILE_STRUCTURES), (s) => !(s instanceof StructureController) && !(s instanceof StructureRampart)), (s) => (s instanceof StructureTower ? 1 : (s instanceof StructureSpawn ? 2 : 3)));
+                    if (attackRoom) {
+                        structures = _.sortBy(_.filter(attackRoom.find(FIND_HOSTILE_STRUCTURES), (s) => !(s instanceof StructureController) && !(s instanceof StructureRampart)), (s) => (s instanceof StructureTower ? 1 : (s instanceof StructureSpawn ? 2 : 3)));
 
                         // Dismantle towers, spawns, and any remaining structures.
                         if (structures.length > 0) {
@@ -242,7 +250,7 @@ var Cache = require("cache"),
                     });
 
                     // Rally to army's attack location.
-                    task = new TaskRally(Memory.army[army].attackRoom);
+                    task = new TaskRally(attackRoomName);
                     _.forEach(creepsWithNoTask, (creep) => {
                         task.canAssign(creep);
                     });

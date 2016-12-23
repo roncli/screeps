3dbcbddf515fc493361c89e57675f22b09eafce7
Ronald M. Clifford <roncli@roncli.com>
2016-12-23 00:03:25 -0800

    Don't rally on 0/49 tiles if possible.

--- src/task.rally.js
+++ src/task.rally.js
@@ -43,7 +43,15 @@ Rally.prototype.run = function(creep) {
     // Rally to the rally point.
     range = creep.room.name === this.rallyPoint.roomName || !(this.rallyPoint instanceof RoomPosition) || (this.rallyPoint.pos && creep.room.name === this.rallyPoint.pos.roomName) ? (this.range || 0) : 20;
     if (creep.pos.getRangeTo(this.rallyPoint) <= range) {
-        if (_.filter(creep.pos.lookFor(LOOK_STRUCTURES), (s) => s instanceof StructureRoad).length > 0) {
+        if (creep.pos.x === 0) {
+            creep.move(RIGHT);
+        } else if (creep.pos.x === 49) {
+            creep.move(LEFT);
+        } else if (creep.pos.y === 0) {
+            creep.move(BOTTOM);
+        } else if (creep.pos.y === 49) {
+            creep.move(TOP);
+        } else if (_.filter(creep.pos.lookFor(LOOK_STRUCTURES), (s) => s instanceof StructureRoad).length > 0) {
             creep.move(Math.floor(Math.random() * 8));
         }
     } else {

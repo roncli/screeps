39ee624d9467afbc15711fc2986f62ebbea19593
Ronald M. Clifford <roncli@roncli.com>
2016-12-23 19:13:03 +0000

    Fix army healers.

--- src/role.armyHealer.js
+++ src/role.armyHealer.js
@@ -29,10 +29,13 @@ var Cache = require("cache"),
 
             var army = Memory.army[armyName],
                 healerUnits = army.healer.units,
+                body = [],
+                boostRoom, labsInUse, count, spawnToUse, name, labsToBoostWith;
+                
+            if (army.boostRoom) {
                 boostRoom = Game.rooms[army.boostRoom],
                 labsInUse = boostRoom.memory.labsInUse,
-                body = [],
-                count, spawnToUse, name, labsToBoostWith;
+            }
 
             // Fail if all the spawns are busy.
             if (_.filter(Game.spawns, (s) => !s.spawning && !Cache.spawning[s.id]).length === 0) {

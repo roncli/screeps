2cd64ca6d6b632ea65ef24f94125ea497fc9a843
Ronald M. Clifford <roncli@roncli.com>
2016-12-28 06:04:48 -0800

    supportRoom -> room

--- src/role.storer.js
+++ src/role.storer.js
@@ -81,7 +81,7 @@ var Cache = require("cache"),
             }
 
             // Create the creep from the first listed spawn that is available.
-            spawnToUse = _.sortBy(_.filter(Game.spawns, (s) => !s.spawning && !Cache.spawning[s.id] && s.room.energyAvailable >= Utilities.getBodypartCost(body) && s.room.memory.region === supportRoom.memory.region), (s) => s.room.name === supportRoomName ? 0 : 1)[0];
+            spawnToUse = _.sortBy(_.filter(Game.spawns, (s) => !s.spawning && !Cache.spawning[s.id] && s.room.energyAvailable >= Utilities.getBodypartCost(body) && s.room.memory.region === room.memory.region), (s) => s.room.name === roomName ? 0 : 1)[0];
             if (!spawnToUse) {
                 return false;
             }

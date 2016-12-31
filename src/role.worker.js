ddf88da6c747af4a9f5ae1cccca907274e6d5bcd
Ronald M. Clifford <roncli@roncli.com>
2016-12-31 14:52:29 -0800

    Don't check for boosts if lab doesn't exist.

--- src/role.worker.js
+++ src/role.worker.js
@@ -144,7 +144,7 @@ var Cache = require("cache"),
             // If not yet boosted, go get boosts.
             _.forEach(_.filter(creepsWithNoTask, (c) => c.memory.labs && c.memory.labs.length > 0), (creep) => {
                 var lab = _.filter(room.memory.labsInUse, (l) => l.id === creep.memory.labs[0])[0];
-                if (Game.getObjectById(creep.memory.labs[0]).mineralType === lab.resource && Game.getObjectById(creep.memory.labs[0]).mineralAmount >= lab.amount) {
+                if (lab && Game.getObjectById(creep.memory.labs[0]).mineralType === lab.resource && Game.getObjectById(creep.memory.labs[0]).mineralAmount >= lab.amount) {
                     var task = new TaskRally(creep.memory.labs[0]);
                     task.canAssign(creep);
                     assigned.push(creep.name);

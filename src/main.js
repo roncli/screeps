5792d40121d8bd94c8e3a0ca26e5b8fae98c8726
Ronald M. Clifford <roncli@roncli.com>
2017-01-02 18:12:08 -0800

    Spelling is going to kill me.

--- src/main.js
+++ src/main.js
@@ -646,10 +646,10 @@ var profiler = require("screeps-profiler"),
                     Cache.log.rooms[roomName].labEnergyCapacity = _.sum(_.map(labs, (l) => l.energyCapacity));
 
                     Cache.log.rooms[roomName].nukerEnergy = _.sum(_.map(nukers, (n) => n.energy));
-                    Cache.log.rooms[roomName].nukerEnergyCapacity = _.sum(_.map(nukres, (l) => n.energyCapacity));
+                    Cache.log.rooms[roomName].nukerEnergyCapacity = _.sum(_.map(nukers, (l) => n.energyCapacity));
 
                     Cache.log.rooms[roomName].nukerGhodium = _.sum(_.map(nukers, (n) => n.ghodium));
-                    Cache.log.rooms[roomName].nukerGhodiumCapacity = _.sum(_.map(nukres, (l) => n.ghodiumCapacity));
+                    Cache.log.rooms[roomName].nukerGhodiumCapacity = _.sum(_.map(nukers, (l) => n.ghodiumCapacity));
 
                     Cache.log.rooms[roomName].powerSpawnPower = _.sum(_.map(powerSpawns, (s) => s.power));
                     Cache.log.rooms[roomName].powerSpawnPowerCapacity = _.sum(_.map(powerSpawns, (s) => s.powerCapacity));

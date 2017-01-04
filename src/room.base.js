3fc0c36296b2b56cecee4e92ec31841f7c97b26a
Ronald M. Clifford <roncli@roncli.com>
2017-01-04 20:03:16 +0000

    Get the creeps in a room.

--- src/room.base.js
+++ src/room.base.js
@@ -329,7 +329,7 @@ Base.prototype.tasks = function(room) {
         dismantlers = Utilities.creepsWithNoTask(Cache.creepsInRoom("dismantler", room)).length > 0,
         collectors = Utilities.creepsWithNoTask(Cache.creepsInRoom("collector", room)).length > 0,
         upgraders = Utilities.creepsWithNoTask(Cache.creepsInRoom("upgarder", room)).length > 0,
-        noWorkers = (Cache.creepsInRoom("worker").length + Cache.creepsInRoom("collector").length) === 0;
+        noWorkers = (Cache.creepsInRoom("worker", roomlength + Cache.creepsInRoom("collector", room).length) === 0;
         tasks = {
             build: {
                 tasks: (workers || collectors) ? TaskBuild.getTasks(room) : []

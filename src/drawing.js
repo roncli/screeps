7768fed72326381c81f5d76863a43a8a5cc5589f
Ronald M. Clifford <roncli@roncli.com>
2017-02-06 21:39:42 -0800

    this -> room.visual

--- src/drawing.js
+++ src/drawing.js
@@ -3,7 +3,7 @@ var Drawing = {
         if (options.showMax === undefined) {
             options.showMax = true;
         }
-        this
+        room.visual
             .rect(x, y, h, w, {fill: options.background})
             .rect(x, y, w * value / max, h, {fill: options.bar})
             .text((options.label ? options.label + " " : "") + value.toFixed(0) + (options.showMax ? "/" + max.toFixed(0) : "") + (options.showDetails ? " (" + (100 * value / max).toFixed(3) + "%) " + (max - value).toFixed(0) + " to go" : ""), x + w / 2, y + h / 2 - 0.25, {align: "center", color: options.color});

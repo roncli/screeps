3f450204bda9dc1a9a12e6d70493c1d8fa9ce526
Ronald M. Clifford <roncli@roncli.com>
2017-02-07 18:18:40 -0800

    Add sparkline function!

--- src/drawing.js
+++ src/drawing.js
@@ -10,6 +10,12 @@ var Drawing = {
             .rect(x, y, w, h, {fill: options.background})
             .rect(x, y, w * Math.min(value / max, 1), h, {fill: options.bar})
             .text((options.label ? options.label + " " : "") + value.toFixed(options.valueDecimals) + (options.showMax ? "/" + max.toFixed(0) : "") + (options.showDetails ? " (" + (100 * value / max).toFixed(3) + "%) " + (max - value).toFixed(0) + " to go" : ""), x + w / 2, y + h / 2 + 0.25, {align: "center", color: options.color});
+    },
+
+    sparkline: (room, x, y, w, h, values, options) => {
+        _.forEach(options, (option) => {
+            room.visual.poly(_.map(values, (v, i) => [x + w * (i / (values.length - 1)), y + h * (v[option.key] / (option.max - option.min))]), option);
+        });
     }
 };
 

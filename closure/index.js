662909ab98454d8125e11438be20d5a4dc49be38
Ronald M. Clifford <roncli@roncli.com>
2017-05-02 07:13:49 -0700

    Simplify closure.

--- closure/index.js
+++ closure/index.js
@@ -2,11 +2,7 @@ var fs = require("fs"),
     modConcat = require("module-concat"),
     compile = require("google-closure-compiler-js").compile;
 
-modConcat("../src/main.js", "./main.js", {paths: ["../src"]}, (err, stats) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+modConcat("../src/main.js", "./main.js", {paths: ["../src"]}, () => {
     fs.readFile("./main.js", {encoding: "utf8"}, (err, data) => {
         fs.writeFile("../bin/main.js", compile({jsCode: [{src: data}]}).compiledCode, () => {
             console.log("Complete.");

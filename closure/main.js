65efbc7ee99c1b86af5b3bd96c8454170ff977cc
Ronald M. Clifford <roncli@roncli.com>
2017-05-07 21:46:58 -0700

    Update to commands.

--- closure/main.js
+++ closure/main.js
@@ -2159,144 +2159,218 @@ return module.exports;
 /********** Start module 4: ../src/commands.js **********/
 __modules[4] = function(module, exports) {
 var Cache = __require(3,4);
-
+/**
+ * Commands intended to be used in the Screeps console.  Can be used elsewhere as well.
+ */
 class Commands {
-    static setRoomType(name, options) {
-        if (options === undefined) {
-            delete Memory.rooms[name].roomType;
+    /**
+     * Adds an ally.  All creeps belonging to this user will not be attacked.
+     * @param {string} name The ally to add.
+     */
+    static addAlly(name) {
+        Memory.allies.push(name);
+    }
+    /**
+     * Adds a sign to a room.  When a reserver or upgrader is near the controller, it will apply the sign.
+     * @param {string} roomName The name of the room to sign.
+     * @param {string} text The text to sign.
+     */
+    static addSign(roomName, text) {
+        if (!Memory.signs) {
+            Memory.signs = {};
+        }
+        if (text) {
+            Memory.signs[roomName] = text;
         } else {
-            if (!Memory.rooms[name]) {
-                Memory.rooms[name] = {};
-            }
-            Memory.rooms[name].roomType = options;
+            delete Memory.signs[roomName];
         }
     }
-
-    static claimRoom(fromRoom, toRoom, claim) {
-        if (!Memory.maxCreeps.claimer) {
-            Memory.maxCreeps.claimer = {};
-        }
-
-        if (!Memory.maxCreeps.claimer[fromRoom]) {
-            Memory.maxCreeps.claimer[fromRoom] = {};
-        }
-        
-        if (claim) {
-            Memory.maxCreeps.claimer[fromRoom][toRoom] = true;
-        } else {
-            delete Memory.maxCreeps.claimer[fromRoom][toRoom];
-        }
-    }
-
-    static attackRoom(fromRoom, toRoom, attack) {
+    /**
+     * Attacks the controller in a room.
+     * @param {string} fromRoomName The name of the room to spawn the converter from.
+     * @param {string} toRoomName The name of the room to attack the controller in.
+     * @param {bool} attack Whether to attack the room's controller or not.
+     */
+    static attackRoomController(fromRoomName, toRoomName, attack) {
         if (!Memory.maxCreeps.converter) {
             Memory.maxCreeps.converter = {};
         }
 
-        if (!Memory.maxCreeps.converter[fromRoom]) {
-            Memory.maxCreeps.converter[fromRoom] = {};
+        if (!Memory.maxCreeps.converter[fromRoomName]) {
+            Memory.maxCreeps.converter[fromRoomName] = {};
         }
         
         if (attack) {
-            Memory.maxCreeps.converter[fromRoom][toRoom] = true;
+            Memory.maxCreeps.converter[fromRoomName][toRoomName] = true;
         } else {
-            delete Memory.maxCreeps.converter[fromRoom][toRoom];
+            delete Memory.maxCreeps.converter[fromRoomName][toRoomName];
         }
     }
-    static claimMine(room) {
-        if (Game.rooms[room] && Cache.creeps[room]) {
-            _.forEach(Cache.creeps[room].remoteReserver, (creep) => {
-                creep.claimController(Game.rooms[room].controller);
+    /**
+     * Directs creeps to avoid a room.
+     * @param {string} room The name of the room to avoid.
+     * @param {bool} avoid Whether to avoid the room or not.
+     */
+    static avoidRoom(roomName, avoid) {
+        if (avoid && Memory.avoidRooms.indexOf(roomName) === -1) {
+            Memory.avoidRooms.push(roomName);
+        }
+        if (!avoid) {
+            _.remove(Memory.avoidRooms, (r) => r === roomName);
+        }
+    }
+    /**
+     * Directs creeps to avoid a square.
+     * @param {number} x The X coordinate of the square to avoid.
+     * @param {number} y The Y coordinate of the square to avoid.
+     * @param {string} roomName The name of the room of the square to avoid.
+     * @param {bool} avoid Whether to avoid the square or not.
+     */
+    static avoidSquare(x, y, roomName, avoid) {
+        if (avoid) {
+            if (!Memory.avoidSquares[roomName]) {
+                Memory.avoidSquares[roomName] = [];
+            }
+            Memory.avoidSquares[roomName].push({x: x, y: y});
+        }
+        if (!avoid) {
+            if (Memory.avoidSquares[roomName]) {
+                _.remove(Memory.avoidSquares[roomName], (s) => s.x === x && s.y === y);
+            }
+        }
+    }
+    /**
+     * Claim a room that's currently being reserved.  Only works if you already have a reserver on the controller.
+     * @param {string} roomName The name of the room to claim.
+     */
+    static claimMine(roomName) {
+        if (Game.rooms[roomName] && Cache.creeps[roomName]) {
+            _.forEach(Cache.creeps[roomName].remoteReserver, (creep) => {
+                creep.claimController(Game.rooms[roomName].controller);
             });
         }
     }
+    /**
+     * Claims a room.
+     * @param {string} fromRoomName The name of the room to spawn the claimer from.
+     * @param {string} toRoomName The name of the room to claim.
+     * @param {bool} claim Whether to claim the room or not.
+     */
+    static claimRoom(fromRoomName, toRoomName, claim) {
+        if (!Memory.maxCreeps.claimer) {
+            Memory.maxCreeps.claimer = {};
+        }
 
-    static dismantle(x, y, room) {
+        if (!Memory.maxCreeps.claimer[fromRoomName]) {
+            Memory.maxCreeps.claimer[fromRoomName] = {};
+        }
+        
+        if (claim) {
+            Memory.maxCreeps.claimer[fromRoomName][toRoomName] = true;
+        } else {
+            delete Memory.maxCreeps.claimer[fromRoomName][toRoomName];
+        }
+    }
+    /**
+     * Creates an army.
+     * @param {string} armyName The name of the army.
+     * @param {object} options The options for the army.
+     */
+    static createArmy(armyName, options) {
+        if (options === undefined) {
+            delete Memory.army[armyName];
+        } else {
+            Memory.army[armyName] = options;
+            Memory.army[armyName].directive = "preparing";
+        }
+    }
+    /**
+     * Dismantle a location within a room.
+     * @param {number} x The X coordinate of the object to dismantle.
+     * @param {number} y The Y coordinate of the object to dismantle.
+     * @param {string} roomName The room to dismantle in.
+     */
+    static dismantle(x, y, roomName) {
         if (!Memory.dismantle) {
             Memory.dismantle = {};
         }
 
-        if (!Memory.dismantle[room]) {
-            Memory.dismantle[room] = [];
+        if (!Memory.dismantle[roomName]) {
+            Memory.dismantle[roomName] = [];
         }
 
-        Memory.dismantle[room].push({x: x, y: y});
+        Memory.dismantle[roomName].push({x: x, y: y});
     }
-
-    static stopCreep(name) {
-        if (Game.creeps[name]) {
-            Game.creeps[name].memory.stop = true;
+    /**
+     * Recover from an emergency.
+     */
+    static recover() {
+        _.forEach(Game.spawns, (spawn) => {spawn.createCreep([MOVE, MOVE, CARRY, CARRY, CARRY, CARRY], `storer-emerg-${spawn.room.name}-${spawn.name}`, {role: "storer", home: spawn.room.name})});
+    }
+    /**
+     * Removes an ally.
+     * @param {string} name The ally to remove.
+     */
+    static removeAlly(name) {
+        _.pull(Memory.allies, name);
+    }
+    /**
+     * Resets a wartime cost matrix for a room.  It will be automatically recalculated.
+     * @param {string} roomName The name of the room to reset the base matrix for.
+     */
+    static resetBaseMatrix(roomName) {
+        Memory.baseMatrixes[roomName] = {};
+    }
+    /**
+     * Set a container's source.  Useful when you want to have a container for a source be at a location different than default, ie: E36N11.
+     * @param {string} containerId The container ID to assign to a source.
+     * @param {string} sourceId The source ID to assign to a container.
+     */
+    static setContainerSource(containerId, sourceId) {
+        Memory.containerSource[containerId] = sourceId;
+    }
+    /**
+     * Set the room type.  Options should be an object containing at least a "type" key.
+     * @param {string} roomName The name of the room.
+     * @param {string} region The region of the room.
+     * @param {object} options The options for the room.
+     */
+    static setRoomType(roomName, region, options) {
+        if (options === undefined) {
+            delete Memory.rooms[roomName].roomType;
+        } else {
+            if (!Memory.rooms[roomName]) {
+                Memory.rooms[roomName] = {};
+            }
+            Memory.rooms[roomName].roomType = options;
+            Memory.rooms[roomName].region = region;
         }
     }
-
-    static startCreep(name) {
-        if (Game.creeps[name]) {
-            delete Game.creeps[name].memory.stop;
-        }
-    }
-
+    /**
+     * Starts all creeps moving again.
+     */
     static startAllCreeps() {
         _.forEach(Game.creeps, (creep) => {
             delete creep.memory.stop;
         });
     }
-    static setContainerSource(containerId, sourceId) {
-        Memory.containerSource[containerId] = sourceId;
-    }
-    static addAlly(name) {
-        Memory.allies.push(name);
-    }
-
-    static removeAlly(name) {
-        _.pull(Memory.allies, name);
-    }
-
-    static createArmy(army, options) {
-        if (options === undefined) {
-            delete Memory.army[army];
-        } else {
-            Memory.army[army] = options;
-            Memory.army[army].directive = "preparing";
+    /**
+     * Starts a creep moving again.
+     * @param {string} name The name of the creep to start moving.
+     */
+    static startCreep(name) {
+        if (Game.creeps[name]) {
+            delete Game.creeps[name].memory.stop;
         }
     }
-
-    static avoidRoom(room, avoid) {
-        if (avoid && Memory.avoidRooms.indexOf(room) === -1) {
-            Memory.avoidRooms.push(room);
+    /**
+     * Stops a creep from moving.
+     * @param {string} name The name of the creep to stop moving.
+     */
+    static stopCreep(name) {
+        if (Game.creeps[name]) {
+            Game.creeps[name].memory.stop = true;
         }
-        if (!avoid) {
-            _.remove(Memory.avoidRooms, (r) => r === room);
-        }
-    }
-
-    static avoidSquare(x, y, room, avoid) {
-        if (avoid) {
-            if (!Memory.avoidSquares[room]) {
-                Memory.avoidSquares[room] = [];
-            }
-            Memory.avoidSquares[room].push({x: x, y: y});
-        }
-        if (!avoid) {
-            if (Memory.avoidSquares[room]) {
-                _.remove(Memory.avoidSquares[room], (s) => s.x === x && s.y === y);
-            }
-        }
-    }
-    static addSign(room, text) {
-        if (!Memory.signs) {
-            Memory.signs = {};
-        }
-        if (text) {
-            Memory.signs[room] = text;
-        } else {
-            delete Memory.signs[room];
-        }
-    }
-    static resetMatrix(room) {
-        Memory.baseMatrixes[room] = {};
-    }
-    static recover() {
-        _.forEach(Game.spawns, (spawn) => {spawn.createCreep([MOVE, MOVE, CARRY, CARRY, CARRY, CARRY], `storer-emerg-${spawn.room.name}-${spawn.name}`, {role: "storer", home: spawn.room.name})});
     }
 }
 

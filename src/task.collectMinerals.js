bc7aa47dd6494681459c937e25143e464b73ef09
Ronald M. Clifford <roncli@roncli.com>
2017-06-05 14:30:18 -0700

    Don't collect minerals if we have energy.

--- src/task.collectMinerals.js
+++ src/task.collectMinerals.js
@@ -47,7 +47,7 @@ class TaskCollectMinerals {
     canAssign(creep) {
         const {object: obj} = this;
 
-        if (!obj || this.amount < 0 || creep.spawning || creep.ticksToLive < 150 || _.sum(creep.carry) === creep.carryCapacity) {
+        if (!obj || this.amount < 0 || creep.spawning || creep.ticksToLive < 150 || _.sum(creep.carry) === creep.carryCapacity || creep.carry[RESOURCE_ENERGY] > 0) {
             return false;
         }
 

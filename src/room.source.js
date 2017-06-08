e3cd84abdf69d20a996d0155391c55af790072d4
Ronald M. Clifford <roncli@roncli.com>
2017-06-08 07:33:17 -0700

    Same for stage 2.

--- src/room.source.js
+++ src/room.source.js
@@ -145,19 +145,21 @@ class RoomSource extends RoomMine {
 
         super.stage2Tasks();
 
-        this.tasks.hostiles = Cache.hostilesInRoom(room);
-        this.tasks.keepers = Cache.sourceKeepersInRoom(room).sort((a, b) => {
-            if (a.ticksToSpawn && !b.ticksToSpawn) {
-                return -1;
-            }
+        if (!room.unobservable) {
+            this.tasks.hostiles = Cache.hostilesInRoom(room);
+            this.tasks.keepers = Cache.sourceKeepersInRoom(room).sort((a, b) => {
+                if (a.ticksToSpawn && !b.ticksToSpawn) {
+                    return -1;
+                }
 
-            if (!a.ticksToSpawn && b.ticksToSpawn) {
-                return 1;
-            }
+                if (!a.ticksToSpawn && b.ticksToSpawn) {
+                    return 1;
+                }
 
-            return a.ticksToSpawn - b.ticksToSpawn;
-        });
-        this.tasks.hurtCreeps = _.filter(Game.creeps, (c) => c.room.name === room.name && c.hits < c.hitsMax).sort((a, b) => a.hits / a.hitsMax - b.hits / b.hitsMax);
+                return a.ticksToSpawn - b.ticksToSpawn;
+            });
+            this.tasks.hurtCreeps = _.filter(Game.creeps, (c) => c.room.name === room.name && c.hits < c.hitsMax).sort((a, b) => a.hits / a.hitsMax - b.hits / b.hitsMax);
+        }
     }
 
     //         #                       ##    ##

fc95bfa38b898cc2a5572a4ed26c195fa24e6b77
Ronald M. Clifford <roncli@roncli.com>
2017-06-08 07:29:31 -0700

    Ensure room is observable.

--- src/room.source.js
+++ src/room.source.js
@@ -51,9 +51,11 @@ class RoomSource extends RoomMine {
 
         super.stage1Tasks();
 
-        this.tasks.hostiles = Cache.hostilesInRoom(room);
-        this.tasks.keepers = Cache.sourceKeepersInRoom(room);
-        this.tasks.hurtCreeps = _.filter(Game.creeps, (c) => c.room.name === room.name && c.hits < c.hitsMax).sort((a, b) => a.hits / a.hitsMax - b.hits / b.hitsMax);
+        if (!room.unobservable) {
+            this.tasks.hostiles = Cache.hostilesInRoom(room);
+            this.tasks.keepers = Cache.sourceKeepersInRoom(room);
+            this.tasks.hurtCreeps = _.filter(Game.creeps, (c) => c.room.name === room.name && c.hits < c.hitsMax).sort((a, b) => a.hits / a.hitsMax - b.hits / b.hitsMax);
+        }
     }
 
     //         #                       #     ##

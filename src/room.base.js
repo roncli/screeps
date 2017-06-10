228ee4a8ddde6dc3da91c95c1c731691ee573ea7
Ronald M. Clifford <roncli@roncli.com>
2017-06-10 03:49:22 -0700

    Change energy thresholds on terminals.

--- src/room.base.js
+++ src/room.base.js
@@ -660,8 +660,8 @@ class RoomBase extends RoomEngine {
             spawns: _.filter(Cache.spawnsInRoom(room), (s) => s.energy < SPAWN_ENERGY_CAPACITY),
             structuresWithEnergy: [...storage && storage.my ? [storage] : [], ..._.filter(Cache.containersInRoom(room), (c) => c.store[RESOURCE_ENERGY] >= 500).sort((a, b) => b.store[RESOURCE_ENERGY] - a.store[RESOURCE_ENERGY])],
             structuresWithMinerals: _.filter(Cache.containersInRoom(room), (c) => c.store[RESOURCE_ENERGY] < _.sum(c.store)).sort((a, b) => _.sum(b.store) - _.sum(a.store)),
-            terminalsCollectEnergy: terminal && (!terminal.my || terminalEnergy >= 5000 && (!roomMemory.buyQueue || !storage || store[RESOURCE_ENERGY] < Memory.marketEnergy || Cache.credits < Memory.minimumCredits)) ? [terminal] : [],
-            terminalsFillWithEnergy: terminal && terminal.my && terminalEnergy < 1000 ? [terminal] : [],
+            terminalsCollectEnergy: terminal && (!terminal.my || terminalEnergy >= 10000 && (!roomMemory.buyQueue || !storage || store[RESOURCE_ENERGY] < Memory.marketEnergy || Cache.credits < Memory.minimumCredits)) ? [terminal] : [],
+            terminalsFillWithEnergy: terminal && terminal.my && terminalEnergy < 5000 ? [terminal] : [],
             towers: _.filter(Cache.towersInRoom(room), (t) => t.energy < TOWER_CAPACITY * 0.8)
         };
 

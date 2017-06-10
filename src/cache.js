5a5c7bebb1b0dd65bcf522755c04f6b6dd20a6e4
Ronald M. Clifford <roncli@roncli.com>
2017-06-09 22:14:07 -0700

    Fix bug with threat.

--- src/cache.js
+++ src/cache.js
@@ -289,7 +289,7 @@ class Cache {
                 })
             })), (value) => value.id);
 
-        memory.threat = _.sum(_.map(_.values(threat), (t) => t.threat));
+        memory.threat = _.sum(_.map(_.values(threat), (t) => t[0].threat));
 
         this.hostiles[roomName].sort((a, b) => threat[b.id].threat - threat[a.id].threat);
 

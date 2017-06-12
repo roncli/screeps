d88348d8137c0a494f140e24a8eb87b257e7dc7b
Ronald M. Clifford <roncli@roncli.com>
2017-06-11 19:01:19 -0700

    Teaking army size for mines.

--- src/room.mine.js
+++ src/room.mine.js
@@ -296,7 +296,7 @@ class RoomMine extends RoomEngine {
             {armies: {[armyName]: army}} = Cache;
 
         if (_.filter(hostiles, (h) => h.owner && h.owner.username !== "Invader").length > 0 && threat && threat > 0) {
-            const maxCreeps = Math.ceil(threat / (BODYPART_COST[ATTACK] * 200));
+            const maxCreeps = Math.ceil(threat / (BODYPART_COST[ATTACK] * 300));
 
             if (army) {
                 army.boostRoom = supportRoomName;
